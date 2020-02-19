a69623ac4e0e4059a17c5b7fec3098dc2bbd5351
dzlab <dzlabs@outlook.com>
2020-02-19 12:03:33 -0800

    remove unnecessary params

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -10,15 +10,5 @@ all_notebooks = [n for n in glob.glob("tutorials/**/*.ipynb", recursive=True)]
 @pytest.mark.parametrize("notebook", sorted(all_notebooks))
 def test_notebooks(notebook):
     """Test Notebooks in the tutorial root folder."""
-    res = pm.execute_notebook(
-        notebook,
-        "/dev/null",
-        parameters={
-            "epochs": 1,
-            "n_test_batches": 5,
-            "n_train_items": 64,
-            "n_test_items": 64,
-        },
-        timeout=300,
-    )
+    res = pm.execute_notebook(notebook, "/dev/null", parameters={}, timeout=300,)
     assert isinstance(res, nbformat.notebooknode.NotebookNode)
