805b1b4112e94efd92d5a7001381dfdbda00f8f2
Jatin Prakash <jatinprakash@Jatins-MacBook-Air.local>
2020-03-15 12:47:28 +0530

    tests for custom doc attr

---
+++ tests/local_mode/test_custom_doc_attr.py
@@ -0,0 +1,17 @@
+import syft as sy
+import torch
+import syfertext
+
+hook = sy.TorchHook(torch)
+me = hook.local_worker
+
+nlp = syfertext.load("en_core_web_lg", owner=me)
+
+def test_add_custom_attr_doc():
+    """Test adding custom attribute to Doc objects"""
+    
+    doc = nlp("Joey doesnt share food")
+    doc.set_attribute(name = 'my_custom_tag', value = 'tag')
+    
+    # check custom tag has been added
+    assert (doc._.my_custom_tag == 'tag')
