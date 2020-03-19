9106b4efac0f5d7010831b17a4aec850d81751d4
jatin <jatinprakash1511@gmail.com>
2020-03-20 00:27:52 +0530

    fix tests

--- tests/local_mode/test_doc_vector.py
+++ tests/local_mode/test_doc_vector.py
@@ -9,19 +9,53 @@ me = hook.local_worker
 nlp = syfertext.load("en_core_web_lg", owner=me)
 
 
-def test_avg_vector_valid_token_is_not_zero():
-    """Test that the average vector of valid tokens is not all zeros"""
+def test_avg_vector_valid_token():
+    """Test that the average vector of valid tokens match the expected result"""
     doc = nlp("on your left")
     actual = doc.vector
-    zeros = np.zeros(actual.shape)
-    # check that at least one cell in actual average vector is not zero
-    assert (actual != zeros).any() == True
+    vectors = None
+
+    # Count the tokens that have vectors
+    vector_count = 0
+    for token in doc:
+        # Get the encypted vector of the token if one exists
+        if token.has_vector:
+            # Increment the vector counter
+            vector_count += 1
+            # cumulate token's vector by summing them
+            vectors = token.vector if vectors is None else vectors + token.vector
+    # if no tokens with vectors were found, just get the default vector (zeros)
+    if vector_count == 0:
+        expected_vector = doc.vocab.vectors.default_vector
+    else:
+        # Create the final Doc vector
+        expected_vector = vectors / vector_count
+
+    # assert that actual and expected results match
+    assert (actual == expected_vector).all()
 
 
-def test_avg_vector_non_valid_token_is_zero():
-    """Test that the average vector of non valid token is all zeros"""
+def test_avg_vector_non_valid():
+    """Test that the average vector of invalid tokens match the expected result"""
     doc = nlp("non-valid-token")
     actual = doc.vector
-    zeros = np.zeros(actual.shape)
-    # check that all cells in actual average vector are zeros
-    assert (actual == zeros).all() == True
+    vectors = None
+
+    # Count the tokens that have vectors
+    vector_count = 0
+    for token in doc:
+        # Get the encypted vector of the token if one exists
+        if token.has_vector:
+            # Increment the vector counter
+            vector_count += 1
+            # cumulate token's vector by summing them
+            vectors = token.vector if vectors is None else vectors + token.vector
+    # if no tokens with vectors were found, just get the default vector (zeros)
+    if vector_count == 0:
+        expected_vector = doc.vocab.vectors.default_vector
+    else:
+        # Create the final Doc vector
+        expected_vector = vectors / vector_count
+
+    # assert that actual and expected results match
+    assert (actual == expected_vector).all()
