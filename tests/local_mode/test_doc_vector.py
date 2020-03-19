529f575a4db8b7ad520429612d7cc1e1a826b45d
jatin <jatinprakash1511@gmail.com>
2020-03-20 00:06:35 +0530

    add tests

---
+++ tests/local_mode/test_doc_vector.py
@@ -0,0 +1,27 @@
+import syft as sy
+import torch
+import syfertext
+import numpy as np
+
+hook = sy.TorchHook(torch)
+me = hook.local_worker
+
+nlp = syfertext.load("en_core_web_lg", owner=me)
+
+
+def test_avg_vector_valid_token_is_not_zero():
+    """Test that the average vector of valid tokens is not all zeros"""
+    doc = nlp("on your left")
+    actual = doc.vector
+    zeros = np.zeros(actual.shape)
+    # check that at least one cell in actual average vector is not zero
+    assert (actual != zeros).any() == True
+
+
+def test_avg_vector_non_valid_token_is_zero():
+    """Test that the average vector of non valid token is all zeros"""
+    doc = nlp("non-valid-token")
+    actual = doc[0].vector
+    zeros = np.zeros(actual.shape)
+    # check that all cells in actual average vector are zeros
+    assert (actual == zeros).all() == True
