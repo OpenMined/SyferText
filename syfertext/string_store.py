69ef88e02df9da0f40f29c07d5a4b3dd918e7016
sachin-101 <sachinkumar04428@gmail.com>
2020-03-20 16:54:09 +0530

    Added store to branch

--- syfertext/string_store.py
+++ syfertext/string_store.py
@@ -8,10 +8,25 @@ from .utils import hash_string
 class StringStore:
 
     def __init__(self, strings=None):
-        """Create the StringStore"""
+        """Create the StringStore object
 
-        self.key_to_string = {}
-        self.string_to_key = {}
+        strings (list): List of Strings to add to StringStore 
+        """
+        self.key_to_str = {}
+        self.str_to_key = {}
+
+        if strings is not None:  # load strings 
+            for word in strings:
+                key = hash_string(word)
+                self.key_to_str[key] = word
+                self.str_to_key[word] = key
+
+
+     def __contains__(self, string):
+        """Check whether a string in in the store"""
+
+        return string in self.str_to_key.keys()
+
 
     def add(self, string):
         """Add a sting to the StringStore
@@ -20,19 +35,24 @@ class StringStore:
         """
 
         if isinstance(string, str):
-            if string in self.key_to_string.values():
-                key = self.string_to_key[string]
+            
+            if string in self:  # store contains string
+                
+                key = self.str_to_key[string]
+            
             else:
                 # get corresponding hash value
                 key = hash_string(string)
 
                 # add string to dictionaries
-                self.string_to_key[string] = key
-                self.key_to_string[key] = string                
+                self.str_to_key[string] = key
+                self.key_to_str[key] = string                
+        
         else:
             raise TypeError(f"string is of type {type(string)}; Expected type str")
         
         return key
+    
     
     def __getitem__(self, string_or_id):
         """Retrieve a string from a given hash,  or vice-versa
@@ -43,12 +63,19 @@ class StringStore:
         if isinstance(string_or_id, int):
             # if string_or_id is hash value
             # return correpoding string
-            return self.key_to_string[string_or_id]
+            return self.key_to_str[string_or_id]
         
         elif isinstance(string_or_id, str):
             # if string_or_id is of type string
-            # return correpoding hash value
-        
-            return self.string_to_key[string_or_id]
-        else:
+
+            if string_or_id not in self:    # string not in store
+                key = self.add(string_or_id)
+            else: 
+                key = self.str_to_key[string_or_id]
+
+            return key
+
+        else:   # Wrong type
             raise TypeError(f"key is of type {type(string_or_id)}; Expected type str or int")
+
+   
