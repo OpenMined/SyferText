4feae999be6e4c9682b3d0f26b1fa0b8366a3952
Jatin Prakash <jatinprakash1511@gmail.com>
2020-03-22 21:44:40 +0530

    black test doc

--- tests/local_mode/test_doc.py
+++ tests/local_mode/test_doc.py
@@ -12,32 +12,32 @@ nlp = syfertext.load("en_core_web_lg", owner=me)
 
 def test_avg_vector_valid_token():
     """Test that the average vector of valid tokens match the expected result"""
-    
+
     doc = nlp("on your left")
     actual = doc.vector
     vectors = None
 
     # Count the tokens that have vectors
     vector_count = 0
-    
+
     for token in doc:
-        
+
         # Get the vector of the token if one exists
         if token.has_vector:
-            
+
             # Increment the vector counter
             vector_count += 1
-            
+
             # cumulate token's vector by summing them
             vectors = token.vector if vectors is None else vectors + token.vector
-    
+
     # if no tokens with vectors were found, just get the default vector (zeros)
     if vector_count == 0:
-        
+
         expected_vector = doc.vocab.vectors.default_vector
-    
+
     else:
-        
+
         # Create the final Doc vector
         expected_vector = vectors / vector_count
 
@@ -47,32 +47,32 @@ def test_avg_vector_valid_token():
 
 def test_avg_vector_non_valid():
     """Test that the average vector of invalid tokens match the expected result"""
-    
+
     doc = nlp("non-valid-token")
     actual = doc.vector
     vectors = None
 
     # Count the tokens that have vectors
     vector_count = 0
-    
+
     for token in doc:
-        
+
         # Get the vector of the token if one exists
         if token.has_vector:
-            
+
             # Increment the vector counter
             vector_count += 1
-            
+
             # cumulate token's vector by summing them
             vectors = token.vector if vectors is None else vectors + token.vector
-    
+
     # if no tokens with vectors were found, just get the default vector (zeros)
     if vector_count == 0:
-        
+
         expected_vector = doc.vocab.vectors.default_vector
-    
+
     else:
-        
+
         # Create the final Doc vector
         expected_vector = vectors / vector_count
 
