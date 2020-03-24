1d04390f441639a3edeee1305d4291b5be081a11
Nilanshrajput <2017csb1092@iitrpr.ac.in>
2020-03-24 18:18:01 +0530

    added test for infix tokenization, eceptions

--- tests/local_mode/test_language.py
+++ tests/local_mode/test_language.py
@@ -20,25 +20,44 @@ def test_vector_valid_token_is_not_zero():
 
 def test_vector_non_valid_token_is_zero():
     """Test that the vector of non valid token is all zeros"""
+
     doc = nlp("non-valid-token")
     actual = doc[0].vector
     zeros = np.zeros(actual.shape)
+
     # check that all cells in actual vector are zeros
     assert (actual == zeros).all() == True
 
 
 def test_length_of_doc():
     """Test that tokenizer creates right number of tokens are created"""
+
     doc = nlp("I #love  app-le.")
     actual = 8
     tokens_created = len(doc)
+
     # check that all cells in actual vector are zeros
     assert actual == tokens_created
 
 
 def test_correctness_of_tokens_created():
     """Test that tokens created by tokenizer are correct"""
-    doc = nlp("I #love  app-le!")
-    actual_tokens = ["I", "#", "love", " ", "app", "-", "le", "!"]
+
+    doc = nlp("I 'love  app-le!")
+    actual_tokens = ["I", "'", "love", " ", "app", "-", "le", "!"]
+
+    # check that tokenizer is correctly tokenizing the text.
     for i, token_created in enumerate(doc):
         assert str(token_created) == actual_tokens[i]
+
+    # check correct infix are tokenized only
+    str1 = "Hell#o"  # this string does not contains infix.
+    str2 = "Hell-o"  # This string contains logical infix.
+
+    assert len(nlp(str1)) == 1  # ['Hell#o']
+    assert len(nlp(str2)) == 3  # ['Hell', '-', 'o']
+
+    # check exception cases are tokenized correctly
+    excep = "U.S.A"
+
+    assert len(nlp(excep)) == 1  # ['U.S.A']
