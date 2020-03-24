9785b136557dcae2d7d263c58b3bf7cbb1cf874d
sachin-101 <sachinkumar04428@gmail.com>
2020-03-24 23:16:02 +0530

    Improved error messages

--- syfertext/exceptions.py
+++ syfertext/exceptions.py
@@ -2,12 +2,17 @@ class ObjectNotCallableError(Exception):
     """Exception raised when a wrong object is added to pipeline
     This exception is raised when an object which doesn't
     has a __call__ attribute is added to the pipeline
-    by passsing as a parameter in add_pipe method
+    by passing as a parameter in add_pipe method
     """
 
     def __init__(self, param_name):
-        message = f"Argument `{param_name}` is not a callable."
-        super.__init__(message)
+        message = (
+            "Passed in object for argument "
+            + param_name
+            + " is not a callable."
+            + "Please provide a callable object."
+        )
+        super().__init__(message)
 
 
 class DuplicateNameError(Exception):
@@ -18,10 +23,9 @@ class DuplicateNameError(Exception):
 
     def __init__(self, name):
         message = (
-            "Pipeline component with"
+            "Pipeline component with name:"
             + name
-            + "that you have chosen is already"
-            + "used by another pipeline component."
+            + " already exists in pipeline."
             + "Please use a different name."
         )
         super().__init__(message)
@@ -33,10 +37,11 @@ class InvalidPositionError(Exception):
 
     def __init__(self, **kwargs):
         message = (
-            "Component can not be added to pipeline" + "You have provided arguments values",
-            kwargs
-            + "which makes the position of new component ambiguous"
-            + "Please provide a valid set of arguments to the method call",
+            "Component can not be added to pipeline. "
+            + "You have provided arguments values "
+            + str(kwargs)
+            + " which makes the position of new component ambiguous."
+            + "Please provide a valid set of arguments."
         )
         super().__init__(message)
 
@@ -46,5 +51,11 @@ class PipelineComponentNotFoundError(Exception):
     """
 
     def __init__(self, name):
-        message = "No pipeline component with specified" + name + "was found in Pipeline"
+        message = (
+            "No component with specified name: "
+            + name
+            + " was found in Pipeline."
+            + "Please provide name of a component"
+            + "available in pipeline"
+        )
         super().__init__(message)
