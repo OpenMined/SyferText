ba020dadefa34e46ed0b5bdf325fbe188d047696
sachin-101 <sachinkumar04428@gmail.com>
2020-03-24 23:52:18 +0530

    Added tests for custom exceptions

---
+++ tests/test_exceptions.py
@@ -0,0 +1,69 @@
+import syft as sy
+import torch
+import syfertext
+
+from syfertext.pipeline import SimpleTagger
+from syfertext.exceptions import DuplicateNameError
+from syfertext.exceptions import ObjectNotCallableError
+from syfertext.exceptions import InvalidPositionError
+from syfertext.exceptions import PipelineComponentNotFoundError
+
+hook = sy.TorchHook(torch)
+me = hook.local_worker
+
+nlp = syfertext.load("en_core_web_lg", owner=me)
+
+# Create a simple tagger
+tagger = SimpleTagger(attribute="noun", lookups=["I", "You"])
+
+
+def test_duplicate_name_error():
+
+    try:
+        name = "my tagger"
+        nlp.add_pipe(component=tagger, name=name)
+
+        # adding component with duplicate name
+        nlp.add_pipe(component=tagger, name=name)
+
+    except Exception as e:
+        assert isinstance(e, DuplicateNameError)
+
+
+def test_object_not_callable_error():
+
+    try:
+        not_callable_object = "a string"
+
+        # adding component str which can't be called
+        nlp.add_pipe(component=not_callable_object)
+
+    except Exception as e:
+        assert isinstance(e, ObjectNotCallableError)
+
+
+def test_pipeline_component_not_found_error():
+
+    try:
+        # component with passed name is not available in pipeline
+        nlp.remove_pipe("not my tagger")
+
+    except Exception as e:
+        assert isinstance(e, PipelineComponentNotFoundError)
+
+
+def test_invalid_position_error():
+
+    try:
+        # Setting last as False but not providing any other position
+        nlp.add_pipe(component=tagger, name="my new tagger", last=False)
+
+    except Exception as e:
+        assert isinstance(e, InvalidPositionError)
+
+    try:
+        # Passing more than one position as True
+        nlp.add_pipe(component=tagger, name="my new tagger", last=True, first=True)
+
+    except Exception as e:
+        assert isinstance(e, InvalidPositionError)
