394430b618971d38f6c79ad6d9dff4c431f68707
Jatin Prakash <jatinprakash1511@gmail.com>
2020-03-26 13:18:05 +0530

    small test fix

--- tests/local_mode/test_token.py
+++ tests/local_mode/test_token.py
@@ -12,7 +12,7 @@ nlp = syfertext.load("en_core_web_lg", owner=me)
 def test_valid_token_norm_is_not_zero():
     """Test that the norm of a valid token is not zero"""
     doc = nlp("banana")
-    norm = doc[0].vector_norm
+    norm = doc[0].vector_norm.item()
 
     # check that norm is not zero for valid token
     assert norm != 0.0
@@ -21,7 +21,7 @@ def test_valid_token_norm_is_not_zero():
 def test_non_valid_token_norm_is_zero():
     """Test that the norm of a invalid token is zero"""
     doc = nlp("not-valid-token")
-    norm = doc[0].vector_norm
+    norm = doc[0].vector_norm.item()
 
     # check that norm is zero for invalid token
     assert norm == 0.0
@@ -33,7 +33,7 @@ def test_similarity_bw_valid_tokens():
     token1 = doc[0]
     token2 = doc[1]
 
-    sim = token1.similarity(token2)
+    sim = token1.similarity(token2).item()
 
     # check that similarity is not zero for two valid unequal token
     assert sim != 0.0
@@ -45,7 +45,7 @@ def test_similarity_bw_same_valid_tokens():
     token1 = doc[0]
     token2 = doc[1]
 
-    sim = token1.similarity(token2)
+    sim = token1.similarity(token2).item()
 
     # check that norm is one for equal valid tokens
     assert sim == 1.0
