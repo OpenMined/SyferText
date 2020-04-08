c1ca2afc09de3071bc4a420d0f2c7fd6e6eef9f2
sachin-101 <sachinkumar04428@gmail.com>
2020-04-08 14:30:55 +0530

    Improved documentation on pipeline
    Made it more beginner friendly.
    
    Added test for number of subpipelines created
    
    Corrected Documentation
    
    Incorporated suggested changes

--- tests/distributed_mode/test_pipeline.py
+++ tests/distributed_mode/test_pipeline.py
@@ -12,9 +12,85 @@ hook = sy.TorchHook(torch)
 me = hook.local_worker
 
 
-def test_subpipeline_is_not_recreated_in_remote_workers():
+# Create components which can be added to a text processing pipeline
+noun_tagger = SimpleTagger(attribute="noun", lookups=["syerftext", "pysyft"], tag=True)
+verb_tagger = SimpleTagger(attribute="verb", lookups=["build", "learn"], tag=True)
+pronoun_tagger = SimpleTagger(attribute="pronoun", lookups=["he", "she"], tag=True)
+adjective_tagger = SimpleTagger(attribute="adjective", lookups=["secure"], tag=True)
+
+
+def test_addition_and_removal_of_pipeline_components():
+    """Test the add_pipe and remove_pipe methods.
     """
-    Test that the a subpipeline at a given index is not recreated in remote workers after it
+
+    nlp = syfertext.load("en_core_web_lg", owner=me)
+
+    # Add the pipeline components to SyferText pipeline
+    nlp.add_pipe(noun_tagger, name="noun tagger")
+    nlp.add_pipe(verb_tagger, name="verb tagger")
+
+    # Note: Tokenizer is always the first component in any pipeline and
+    # is added by default to the nlp.pipeline_template.
+    # So the current state of the nlp.pipeline_template should be like this
+    # nlp.pipeline_template = [{'remote': True, 'name': 'tokenizer'},
+    #                          {'remote': False, 'name': 'noun tagger'},
+    #                          {'remote': False, 'name': 'verb tagger'}]
+    assert len(nlp.pipeline_template) == 3
+
+    # Remove noun tagger from the pipeline
+    nlp.remove_pipe(name="noun tagger")
+
+    # Assert pipeline has two components
+    assert len(nlp.pipeline_template) == 2
+
+
+def test_number_of_subpipelines_created_with_pipes_of_different_remote_values():
+    """Test the number of subpipelines created when we use pipe components
+    with different remote values.
+    """
+
+    nlp = syfertext.load("en_core_web_lg", owner=me)
+
+    # Add the pipeline components to SyferText pipeline
+    nlp.add_pipe(noun_tagger, name="noun tagger", remote=True)
+    nlp.add_pipe(verb_tagger, name="verb tagger", remote=False)
+    nlp.add_pipe(pronoun_tagger, name="pronoun tagger", remote=False)
+    nlp.add_pipe(adjective_tagger, name="adjective tagger", remote=True)
+
+    # Note: The tokenizer is added by default with remote = True
+    # And the adjacent pipes with same remote value are grouped together
+    # in a single dictionary. So the nlp.subpipeline_templates should be
+    #
+    # nlp.subpipeline_templates = [ {'remote': True, 'names': ['tokenizer','noun tagger']},
+    #                               {'remote': False, 'name': ['verb tagger', 'pronoun tagger']},
+    #                               {'remote': True, 'name': ['adjective tagger']}
+    #                             ]
+
+    remote_subpipelines = [s for s in nlp.subpipeline_templates if (s["remote"] == True)]
+    local_subpipelines = [s for s in nlp.subpipeline_templates if (s["remote"] == False)]
+
+    # Assert subpipeline_templates contains 3 subpipelines,
+    # two with remote = True and one with remote = False
+    assert len(nlp.subpipeline_templates) == 3
+    assert len(remote_subpipelines) == 2
+    assert len(local_subpipelines) == 1
+
+    # Assert the relative order of subpipelines in subpipeline templates
+    for subpipeline, remote_value in zip(nlp.subpipeline_templates, [True, False, True]):
+        assert subpipeline["remote"] == remote_value
+
+    # Make sure the subpipelines contain the correct number of pipes
+    for subpipeline, num_pipes in zip(nlp.subpipeline_templates, [2, 2, 1]):
+        assert len(subpipeline["names"]) == num_pipes
+
+    # Make sure subpipelines contains pipes in the correct order
+    pipes = [["tokenizer", "noun tagger"], ["verb tagger", "pronoun tagger"], ["adjective tagger"]]
+    for subpipeline, pipe_names in zip(nlp.subpipeline_templates, pipes):
+        assert subpipeline["names"] == pipe_names
+
+
+def test_subpipeline_is_not_recreated_in_remote_workers():
+    """Test that the a subpipeline at a given index is not recreated in remote workers after it
     has been initialized once. Each worker contains a single subpipeline, with multiple components.
     """
 
@@ -38,8 +114,7 @@ def test_subpipeline_is_not_recreated_in_remote_workers():
     assert len(subpipelines) == 1
     assert len(documents) == 1
     assert len(nlp.pipeline[0].keys()) == 1
-    assert list(nlp.pipeline[0].keys())[0] == "bob"
-    assert isinstance(nlp.pipeline[0]["bob"], SubPipelinePointer)
+    assert "bob" in nlp.pipeline[0]
 
     # The second time a text owned by `bob` is tokenized, no new `SubPipeline`
     # objects are created. Only a new document on `bob`'s machine.
@@ -59,8 +134,7 @@ def test_subpipeline_is_not_recreated_in_remote_workers():
     assert len(subpipelines) == 1
     assert len(documents) == 1
     assert len(nlp.pipeline[0].keys()) == 2
-    assert list(nlp.pipeline[0].keys())[1] == "alice"
-    assert isinstance(nlp.pipeline[0]["alice"], SubPipelinePointer)
+    assert "alice" in nlp.pipeline[0]
 
     # The third time a text owned by `bob` is tokenized, no new `SupPipeline`
     # objects are created. The `nlp` object still has the same previous pointer
@@ -73,47 +147,6 @@ def test_subpipeline_is_not_recreated_in_remote_workers():
     assert len(nlp.pipeline[0].keys()) == 2
 
 
-def test_subpipeline_component_addition_and_removal_from_pipeline():
-    """Test that on addition of a pipeline component with remote
-    set as True or False, it is added to appropriate subpipeline on
-    remote/local worker"""
-
-    nlp = syfertext.load("en_core_web_lg", owner=me)
-
-    tagger = SimpleTagger(attribute="noun", lookups=["he", "she"], tag=True)
-
-    # Note: If a pipeline component is added to the pipeline with remote set as True,
-    # then it can be sent to a remote worker, if the doc/string object to be processed is on a remote machine
-
-    # Add tagger to the pipeline
-    nlp.add_pipe(tagger, name="my tagger")  # Note: remote = False (default)
-
-    # Now, remote is set to True by default for Tokenizer, hence it is created where the doc/strings is located
-    # Since the remote values of Tokenizer and tagger(added above) are different, tagger is added in another subpipeline
-    # Assert nlp pipeline has two subpipelines
-    assert len(nlp.pipeline) == 2  # Tokenizer is added by default
-
-    # Remove tagger from the pipeline
-    nlp.remove_pipe(name="my tagger")
-
-    # Assert pipeline has single subpipeline
-    assert len(nlp.pipeline) == 1
-
-    # Add tagger to the pipeline
-    nlp.add_pipe(tagger, name="my tagger", remote=True)  # Note: remote = True
-
-    # Now, remote = True by default for Tokenizer, hence it is created where the doc/strings is located
-    # Since the remote values of Tokenizer and tagger are same, tagger is added in the same subpipeline as tokenizer
-    # Assert nlp pipeline has only one subpipeline
-    assert len(nlp.pipeline) == 1
-
-    # Now, If we add another tagger, with remote = False
-    nlp.add_pipe(tagger, name="my_new_tagger", remote=False)
-
-    # Assert nlp pipeline has two subpipelines
-    assert len(nlp.pipeline) == 2
-
-
 def test_pipeline_output():
 
     nlp = syfertext.load("en_core_web_lg", owner=me)
@@ -121,13 +154,13 @@ def test_pipeline_output():
     james = sy.VirtualWorker(hook, id="james")
 
     # Create a PySyft String and send it to remote worker james
-    text_ptr = String("building syfertext").send(james)
+    text_ptr = String("building SyferText").send(james)
 
     # Add tagger with remote = True
-    tagger = SimpleTagger(attribute="noun", lookups=["syfertext"], tag=True)
+    tagger = SimpleTagger(attribute="noun", lookups=["SyferText"], tag=True)
     nlp.add_pipe(tagger, name="noun_tagger", remote=True)
 
-    # Now, Upon processing the text present on james's machine,
+    # Upon processing the text present on james's machine,
     # pipeline Should return a DocPointer to the doc on james's machine
     doc = nlp(text_ptr)
     assert isinstance(doc, DocPointer)
