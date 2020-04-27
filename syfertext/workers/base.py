b2cf7dad800fdac038f9b7bc3c2af0f1389eefa3
sachin-101 <sachinkumar04428@gmail.com>
2020-04-27 21:09:42 +0530

    Added SecureWorker functionality to BaseWorker

--- syfertext/workers/base.py
+++ syfertext/workers/base.py
@@ -9,6 +9,12 @@ from typing import Union, List
 
 
 class ExtendedBaseWorker(BaseWorker):
+    """Extends the PySyft BaseWorker. Adds the functionality to share public key
+    and generate private and secret key. Thus enabling it to participate in
+    Diffie-Hellman key exchange protocol.
+    It can also act as a SecureWorker and perform the DH key exchange protocol.
+    """
+
     def __init__(
         self,
         hook: "FrameworkHook",
@@ -31,50 +37,90 @@ class ExtendedBaseWorker(BaseWorker):
             message_pending_time=message_pending_time,
         )
 
+        # TODO: Make secret and private key's private variables
         self.private_key = None
         self.secret = None
+        self.secret_key = None
 
     def generate_private_key(self, shared_prime):
         """Generates a private key.
-        # TODO: Replace with a pseudo random number generator ??
-        Args:
-            shared_prime:
         """
 
+        # TODO: Replace with a pseudo random number generator ??
         self.private_key = random.randint(1, shared_prime)
 
     def generate_public_key(self, shared_prime, shared_base):
+        """ Generates and returns a public key.
         """
 
-        Args:
-            shared_prime:
-            shared_base:
-
-        Returns:
-
-        """
         if self.private_key is None:
             self.generate_private_key(shared_prime)
 
         public_key = (shared_base ** self.private_key) % shared_prime
         return public_key
 
-    def generate_secret_key(self, received_public_key, shared_prime):
+    def generate_secret_key(self, shared_prime, received_public_key):
+        """ Generates a secret key for the worker using the received_public_key.
         """
-        # TODO: Make secret and private key's private variables
+
+        if self.private_key is None:
+            self.generate_private_key(shared_prime)
+
+        self.secret = (received_public_key ** self.private_key) % shared_prime
+
+        # Convert the secret key to bytes of lengths 16, 24 or 32
+        # Useful for AES encryption
+        self.secret_key = self.secret.to_bytes(32, sys.byteorder)
+
+    @staticmethod
+    def execute_dh_key_exchange(shared_prime, shared_base, workers: List["ExtendedBaseWorker"]):
+        """Executes the Multi-party Diffie-Hellman key exchange protocol.
+        At the end of the protocol each worker has a secret key.
+
+        Please refer to the "Operation with more than two parties" section on
+        https://en.wikipedia.org/wiki/Diffie–Hellman_key_exchange to understand
+        the process in detail.
+
         Args:
-            received_public_key:
-            shared_prime:
-
-        Returns:
-
+            shared_prime: The public prime number. Should be very large to ensure the strength
+                            of encryption.
+            shared_base: The public base integer, typically values 2 and 3 can be used.
+            workers (List): List of BaseWorkers involved in the key exchange process
         """
-        assert self.private_key is not None, "Please generate public key first."
 
-        secret = (received_public_key ** self.private_key) % shared_prime
+        num_workers = len(workers)
 
-        # convert the secret key to bytes of lengths 16, 24 or 32
-        self.secret = secret.to_bytes(32, sys.byteorder)
+        # If there are n workers, at max n-1 exchanges are needed before
+        # generating the secret key for a worker.
+        max_exchanges = num_workers - 1
+
+        for i in range(num_workers):
+
+            pointer = i
+
+            # Generate the first public key
+            public_key = workers[pointer].generate_public_key(shared_prime, shared_base)
+
+            # Keep track of exchanges performed
+            exchanges = 1
+
+            while exchanges < max_exchanges:
+                # Update pointer in circular manner
+                pointer = (pointer + 1) % num_workers
+
+                # Keep passing the public key from one worker to another as the shared_base
+                public_key = workers[pointer].generate_public_key(
+                    shared_prime, shared_base=public_key
+                )
+
+                # Update number of exchanges
+                exchanges += 1
+
+            # Update the pointer in circular manner
+            pointer = (pointer + 1) % num_workers
+
+            # Generate the secret key for the worker at the end of the cycle
+            workers[pointer].generate_secret_key(shared_prime, received_public_key=public_key)
 
     @abstractmethod
     def _send_msg(self, message: bin, location: BaseWorker):
