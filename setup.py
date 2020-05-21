1190997a55debeecec0c392ce5c563e3fa703588
Nilanshrajput <2017csb1092@iitrpr.ac.in>
2020-05-21 09:31:10 +0530

    Files for Publishing syfertext on pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 from setuptools import find_packages
+import os
 
+# Utility function function to read README.md file for long description
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="syfertext",
+    name="syfertext",   
     author="Alan Aboudib",
     author_email="agabudeeb@gmail.com",
     description="A privacy preserving NLP framework",
@@ -13,6 +17,8 @@ setup(
     license="Apache-2.0",
     version="0.0.1",
     packages=find_packages(),
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     install_requires=["tqdm==4.36.1", "mmh3==2.5.1", "syft==0.2.5", "requests==2.22.0"],
     extras_require={
         "test": [
@@ -23,4 +29,5 @@ setup(
             "papermill>=1.2.1",
         ]
     },
+    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
 )
