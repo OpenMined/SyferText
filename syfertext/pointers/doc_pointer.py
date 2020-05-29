9ddff88fc5ea09adb191d45c21db90d532a6c6f3
Sachin Kumar <sachinkumar04428@gmail.com>
2020-05-29 22:08:25 +0530

    Added get_attribute to Token and Doc (#95)
    * Added get_attribute() to Token and Doc
    
    * Added tests for get_attribute and improved previous tests
    
    * removed duplicate __len__ from token
    
    * Added set_attribute(), has_attribute(), remove_attribute() and get_attribute() to DocPointer
    
    * Removed duplicate __getitem__() from DocPointer
    
    * Added tests for custom attributes for remote doc
    
    * Changed file name to test_remote_doc.py
    
    * Deleted tets/distributed_mode/test_doc.py
    
    * Solved workers interference with other tests
    
    * removed custom attributes from DocPointer

--- syfertext/pointers/doc_pointer.py
+++ syfertext/pointers/doc_pointer.py
@@ -23,14 +23,14 @@ class DocPointer(ObjectPointer):
         description: str = None,
     ):
         """Create a Doc Pointer from `location` where the `Doc` object resides and
-        `id_at_location`, the id of the `Doc` object at that location. 
+        `id_at_location`, the id of the `Doc` object at that location.
 
         Args:
             location (BaseWorker): the worker where the `Doc` object resides that this
                 DocPointer will point to.
-            
+
             id_at_location (int or str): the id of the `Doc` object at the `location` worker.
-            
+
             owner (BaseWorker): the owner of the this object ie. `DocPointer`
 
         Returns:
@@ -58,11 +58,11 @@ class DocPointer(ObjectPointer):
 
         Args:
             workers (sequence of BaseWorker): A sequence of remote workers from .
-            crypto_provider (BaseWorker): A remote worker responsible for providing cryptography 
+            crypto_provider (BaseWorker): A remote worker responsible for providing cryptography
             (SMPC encryption) functionalities.
             requires_grad (bool): A boolean flag indicating whether gradients are required or not.
             excluded_tokens (Dict): A dictionary used to ignore tokens of the document based on values
-                of their attributes, the keys are the attributes names and they index, for efficiency, 
+                of their attributes, the keys are the attributes names and they index, for efficiency,
                 sets of values.
                 Example: {'attribute1_name' : {value1, value2}, 'attribute2_name': {v1, v2}, ....}
 
@@ -121,16 +121,16 @@ class DocPointer(ObjectPointer):
 
         Args:
             workers (sequence of BaseWorker): A sequence of remote workers from .
-            crypto_provider (BaseWorker): A remote worker responsible for providing cryptography 
+            crypto_provider (BaseWorker): A remote worker responsible for providing cryptography
             (SMPC encryption) functionalities.
             requires_grad (bool): A boolean flag indicating whether gradients are required or not.
             excluded_tokens (Dict): A dictionary used to ignore tokens of the document based on values
-                of their attributes, the keys are the attributes names and they index, for efficiency, 
+                of their attributes, the keys are the attributes names and they index, for efficiency,
                 sets of values.
                 Example: {'attribute1_name' : {value1, value2}, 'attribute2_name': {v1, v2}, ....}
 
         Returns:
-            Tensor: A SMPC-encrypted tensor representing the array of all vectors in the document 
+            Tensor: A SMPC-encrypted tensor representing the array of all vectors in the document
                 this pointer points to.
         """
 
@@ -152,25 +152,6 @@ class DocPointer(ObjectPointer):
         token_vectors = token_vectors.get()
 
         return token_vectors
-
-    def __getitem__(self, item):
-
-        assert isinstance(item, slice), (
-            "DocPointer object can't return a Token. Please call"
-            "__getitem__ on a slice to get a pointer to a Span residing"
-            "on remote machine."
-        )
-
-        # Create the command
-        command = ("__getitem__", self.id_at_location, [item], {})
-
-        # Send the command
-        span_id = self.owner.send_command(self.location, command)
-
-        # Create a SpanPointer from the span_id
-        span = SpanPointer(location=self.location, id_at_location=span_id, owner=sy.local_worker)
-
-        return span
 
     def __len__(self):
 
