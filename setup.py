7c4c6a6fca5764bb2836752332980bd8274118f4
Alan Aboudib <agabudeeb@gmail.com>
2020-05-30 14:28:16 +0200

    Updated setup script

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license="Apache-2.0",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["tqdm==4.36.1", "mmh3==2.5.1", "syft==0.2.5", "requests==2.22.0"],
+    install_requires=["tqdm==4.36.1", "mmh3==2.5.1", "syft==0.2.6", "requests==2.22.0"],
     extras_require={
         "test": [
             "black>=19.10b0",
