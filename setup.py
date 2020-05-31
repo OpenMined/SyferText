804f8027f9be2f8b0c456570dac700b5fb28729c
Alan Aboudib <agabudeeb@gmail.com>
2020-05-31 21:03:16 +0200

    updated setup script. and AbstractObject import path

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ setup(
     license="Apache-2.0",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["tqdm==4.36.1", "mmh3==2.5.1", "syft", "requests==2.22.0"],
+    install_requires=["tqdm==4.36.1",
+                      "mmh3==2.5.1",
+                      "syft @ git+https://github.com/OpenMined/PySyft@c430345b82cdcbaa7a15f8662498c502c4f9a1eb",
+                      "requests==2.22.0"],
     extras_require={
         "test": [
             "black>=19.10b0",
@@ -23,7 +26,5 @@ setup(
             "papermill>=1.2.1",
         ]
     },
-    dependency_links=[
-        "git+https://github.com/OpenMined/PySyft@328de40c21054575955595dc72f7a4ce9065afc7#egg=syft"
-    ],
+
 )
