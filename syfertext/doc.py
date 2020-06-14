d0129ac187aeadf34783b004c440fdccf63722c3
Sachin Kumar <sachinkumar04428@gmail.com>
2020-06-15 02:41:36 +0530

    Added _get_valid_tokens() to Doc and Span (#114)
    * Added _get_valid_tokens() method
    
    * Removed redundant implementation in vector property
    
    * Refactored span, added _get_valid_tokens()
    
    Removed redundant implementation in vector property
    
    * Refactored doc_pointer.py
    
    * Typo corrected
    
    * Added suggested changes
    
    * Fixed typo
    
    * Incroporated suggested changes
    
    * Update syfertext/doc.py
    
    * Black files
    
    * Black files again :|
    
    Co-authored-by: Alan Aboudib <agabudeeb@gmail.com>

--- syfertext/doc.py
+++ syfertext/doc.py
@@ -13,6 +13,7 @@ from typing import List
 from typing import Dict
 from typing import Set
 from typing import Union
+from typing import Generator
 from .underscore import Underscore
 from .span import Span
 from .pointers.span_pointer import SpanPointer
@@ -176,34 +177,9 @@ class Doc(AbstractObject):
         """Get document vector as an average of in-vocabulary token's vectors
 
         Returns:
-          doc_vector: document vector
+            doc_vector: doc vector
         """
-
-        # Accumulate the vectors here
-        vectors = None
-
-        # Count the tokens that have vectors
-        vector_count = 0
-
-        for token in self:
-
-            # Get the vector of the token if one exists
-            if token.has_vector:
-
-                # Increment the vector counter
-                vector_count += 1
-
-                # Cumulate token's vector by summing them
-                vectors = token.vector if vectors is None else vectors + token.vector
-
-        # If no tokens with vectors were found, just get the default vector(zeros)
-        if vector_count == 0:
-            doc_vector = self.vocab.vectors.default_vector
-        else:
-            # Create the final Doc vector
-            doc_vector = vectors / vector_count
-
-        return doc_vector
+        return self.get_vector()
 
     @property
     def vector_norm(self) -> torch.FloatTensor:
@@ -222,10 +198,10 @@ class Doc(AbstractObject):
 
     def similarity(self, other: "Doc") -> torch.Tensor:
         """Compute the cosine similarity between two Doc vectors.
-        
+
         Args:
             other (Doc): The Doc to compare with.
-        
+
         Returns:
             Tensor: A cosine similarity score. Higher is more similar.
         """
@@ -251,48 +227,31 @@ class Doc(AbstractObject):
                 Example: {'attribute1_name' : {value1, value2}, 'attribute2_name': {v1, v2}, ....}
 
         Returns:
-            doc_vector: document vector ignoring excluded tokens
+            doc_vector: Document vector ignoring excluded tokens.
         """
 
-        # if the excluded_token dict in None all token are included
-        if excluded_tokens is None:
-            return self.vector
-
-        # enforcing that the values of the excluded_tokens dict are sets, not lists.
-        excluded_tokens = {
-            attribute: set(excluded_tokens[attribute]) for attribute in excluded_tokens
-        }
+        # Get the valid tokens which are to be included
+        valid_tokens = self._get_valid_tokens(excluded_tokens)
 
         vectors = None
 
         # Count the tokens that have vectors
         vector_count = 0
 
-        for token in self:
+        for token in valid_tokens:
 
-            # Get the vector of the token if one exists and if token is not excluded
-            include_token = True
-
-            include_token = all(
-                [
-                    token.get_attribute(key) not in excluded_tokens[key]
-                    for key in excluded_tokens.keys()
-                    if token.has_attribute(key)
-                ]
-            )
-
-            if token.has_vector and include_token:
+            if token.has_vector:
                 # Increment the vector counter
                 vector_count += 1
 
-                # Cumulate token's vector by summing them
+                # Accumulate token's vector by summing them
                 vectors = token.vector if vectors is None else vectors + token.vector
 
         # If no tokens with vectors were found, just get the default vector(zeros)
         if vector_count == 0:
             doc_vector = self.vocab.vectors.default_vector
         else:
-            # Create the final Doc vector
+            # The Doc vector, which is the average of all vectors
             doc_vector = vectors / vector_count
         return doc_vector
 
@@ -310,31 +269,15 @@ class Doc(AbstractObject):
                 containing all the vectors.
         """
 
-        # enforcing that the values of the excluded_tokens dict are sets, not lists.
-        if excluded_tokens is not None:
-            excluded_tokens = {
-                attribute: set(excluded_tokens[attribute]) for attribute in excluded_tokens
-            }
+        # Get the valid tokens which are to be included
+        valid_tokens = self._get_valid_tokens(excluded_tokens)
 
         # The list for holding all token vectors.
         token_vectors = []
 
-        for token in self:
-
-            # Get the vector of the token if the token is not excluded
-            include_token = True
-
-            if excluded_tokens is not None:
-                include_token = all(
-                    [
-                        token.get_attribute(key) not in excluded_tokens[key]
-                        for key in excluded_tokens.keys()
-                        if token.has_attribute(key)
-                    ]
-                )
-
-            if include_token:
-                token_vectors.append(token.vector)
+        for token in valid_tokens:
+            # Get the vector of the token
+            token_vectors.append(token.vector)
 
         # Convert to Numpy array.
         token_vectors = np.array(token_vectors)
@@ -419,6 +362,45 @@ class Doc(AbstractObject):
         )
 
         return token_vectors
+
+    def _get_valid_tokens(
+        self, excluded_tokens: Dict[str, Set[object]] = None
+    ) -> Generator[Token, None, None]:
+        """Handy function to handle the logic of excluding tokens while performing operations on Doc.
+
+        Args:
+            excluded_tokens (Dict): A dictionary used to ignore tokens of the document based on values
+                of their attributes.
+                Example: {'attribute1_name' : {value1, value2}, 'attribute2_name': {v1, v2}, ....}
+        Yields:
+            A generator with valid tokens, i.e. tokens which are `not` to be excluded.
+        """
+
+        if excluded_tokens:
+            # Enforcing that the values of the excluded_tokens dict are sets, not lists.
+            excluded_tokens = {
+                attribute: set(excluded_tokens[attribute]) for attribute in excluded_tokens
+            }
+
+            # Iterate over all tokens in doc
+            for token in self:
+
+                # Check if token can be included by comparing its attribute values
+                # to those in excluded_tokens dictionary.
+                include_token = all(
+                    [
+                        token.get_attribute(key) not in excluded_tokens[key]
+                        for key in excluded_tokens.keys()
+                        if token.has_attribute(key)
+                    ]
+                )
+
+                if include_token:
+                    yield token
+        else:
+            # All tokens are included
+            for token in self:
+                yield token
 
     @staticmethod
     def create_pointer(
