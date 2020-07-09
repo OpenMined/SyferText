31371748771e3b89324b374e330864f80f8382fe
Ramesht Shukla <36106177+ramesht007@users.noreply.github.com>
2020-07-09 11:38:45 -0700

    # updated setup.py
    updated versions for SyferText_0.1.0

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 from setuptools import find_packages
+import os
+
+# Utility function to read README.md file for long description
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
@@ -13,8 +18,10 @@ setup(
     license="Apache-2.0",
     version="0.0.1",
     packages=find_packages(),
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     install_requires=[
-        "tqdm==4.36.1",
+        "tqdm==4.47.0",
         "mmh3==2.5.1",
         "syft @ git+https://github.com/OpenMined/PySyft@1eb369ae3a1865789f5809bec59f066ac1cbe58d",
         "requests==2.22.0",
@@ -22,10 +29,11 @@ setup(
     extras_require={
         "test": [
             "black>=19.10b0",
-            "pytest>=5.3.5",
-            "pytest-black>=0.3.8",
+            "pytest>=5.4.3",
+            "pytest-black>=0.3.10",
             "jupyter>=1.0.0",
-            "papermill>=1.2.1",
+            "papermill>=2.1.2",
         ]
     },
+    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
 )
