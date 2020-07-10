26009c29ea12fdbf92560e7dc33d9e7052c1dabf
Ramesht Shukla <36106177+ramesht007@users.noreply.github.com>
2020-07-10 02:30:36 -0700

    # made improvments

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ setup(
     keywords="nlp smpc secure multi-party computation federated learning deep learning artificial intelligence secure model sharin natural language processing spacy spaCy",
     classifier=["Programming Language :: Python :: 3.6", "Operating System :: OS Independent"],
     license="Apache-2.0",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "tqdm==4.47.0",
         "mmh3==2.5.1",
         "syft @ git+https://github.com/OpenMined/PySyft@1eb369ae3a1865789f5809bec59f066ac1cbe58d",
         "requests==2.22.0",
