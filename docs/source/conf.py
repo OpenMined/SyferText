3bc68a06ba4eb30c786143ac93d393de48cf3788
Hersh Dhillon <hershdhillon23@gmail.com>
2020-09-09 01:36:42 +0530

    Removing other formats

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx_autodoc_typehints', 'sphinx.ext.napoleon']
 numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
