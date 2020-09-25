72428f39ecd81ec2a8f5d64acb1de51203e87043
Nilansh Rajput <2017csb1092@iitrpr.ac.in>
2020-09-26 01:10:42 +0530

    Register state and pipeline from remote worker (#185)

--- syfertext/pipeline/pointers/pipeline_pointer.py
+++ syfertext/pipeline/pointers/pipeline_pointer.py
@@ -60,6 +60,9 @@ class PipelinePointer(ObjectPointer):
         # Request the pipeline from the remote worker
         pipeline = self.owner.request_obj(self.id_at_location, self.location)
 
+        # Register it in the object store
+        self.owner.register_obj(pipeline)
+
         return pipeline
 
     def deploy_states(self) -> None:
