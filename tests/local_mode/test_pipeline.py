a7fe8b24e244f01c0d20801c03554c9b3a65f5e1
Jatin Prakash <jatinprakash1511@gmail.com>
2020-11-12 21:31:21 +0530

    fix pipeline re-use tests

--- tests/local_mode/test_pipeline.py
+++ tests/local_mode/test_pipeline.py
@@ -1,46 +1,83 @@
 import syft as sy
-import torch
 from syft.generic.string import String
+import syfertext
 from syfertext.local_pipeline import get_test_language_model
+from syfertext.pipeline.single_label_classifier import AverageDocEncoder, SingleLabelClassifier
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from syfertext.utils import hash_string
 
 hook = sy.TorchHook(torch)
 me = hook.local_worker
+me.is_client_worker = False
 
-alice = sy.VirtualWorker(hook, id="alice")
-bob = sy.VirtualWorker(hook, id="bob")
+alice = sy.VirtualWorker(hook, id='alice')
+bob = sy.VirtualWorker(hook, id='bob')
+james = sy.VirtualWorker(hook, id='james')
+dan = sy.VirtualWorker(hook, id = 'dan')
+bill = sy.VirtualWorker(hook, id = 'bill')
 
+class simpleNet(sy.Plan):
+    def __init__(self):
+        super(simpleNet, self).__init__()
+        self.fc = nn.Linear(300, 2)
+
+    def forward(self, x):
+        logits = self.fc(x)
+        probs = F.relu(logits)
+        return probs, logits
+
+doc_encoder = AverageDocEncoder()
+# Create some temporary labels
+labels = ['A', 'B']
+
+def reset_object_store(worker):
+    keys = list(worker._objects)
+    for k in keys:
+        del worker._objects[k]
 
 def test_pipeline_reuse():
+
+    text1 = String('This string is on alice').send(alice)
+    text2 = String('This string is on james').send(james)
+
     nlp = get_test_language_model()
+    pipe_name = nlp.pipeline_name
 
-    text1 = String('lazy dog jumps over sleepy cat')
+    # Set up SingleLabelClassifier and add to pipeline
+    model = simpleNet()
+    classifier = SingleLabelClassifier(
+        classifier=model, 
+        doc_encoder=doc_encoder,
+        encryption='mpc',
+        labels=labels,
+    )  
 
-    assert (len(nlp.subpipelines) == 0)
-    assert ('me' not in nlp.pipeline)
+    nlp.add_pipe(classifier, name='test_classifier', access={'bob'})
+    nlp.deploy(bob)
 
-    doc = nlp(text1)
+    # Clear my object store
+    reset_object_store(me)
 
-    # only one sub-pipeline will be created
-    assert (len(nlp.subpipelines) == 1)
-    assert (len(nlp.pipeline['me']) == 1)
+    deployed_nlp = syfertext.load(pipe_name)
 
-    subpipeline_hash1 = hash_string(string='me' + 'tokenizer')
-    assert subpipeline_hash1 in nlp.subpipelines
+    doc1 = deployed_nlp(text1)
 
-    text2 = String('a cow says moo')
-    doc = nlp(text2)
+    # Now 2 subpipelines were created, one for Tokenizer at Alice (Subpipeline[Tokenizer]), and one for Classfier at Bob(Subpipeline[SingleLabelClassifier])
+    assert len(deployed_nlp.subpipelines) == 2
 
-    # pipeline will be reused now, since text is on the me worker
-    assert (len(nlp.subpipelines) == 1)
-    assert (len(nlp.pipeline['me']) == 1)
+    subpipeline_hash1 = hash_string(string='alice' + 'tokenizer')
+    assert subpipeline_hash1 in deployed_nlp.subpipelines
 
-    text3 = String('horse runs fast').send(bob)
-    doc = nlp(text3)
+    subpipeline_hash2 = hash_string(string='bob' + 'test_classifier')
+    assert subpipeline_hash2 in deployed_nlp.subpipelines
 
-    subpipeline_hash2 = hash_string(string='bob' + 'tokenizer')
-    assert subpipeline_hash2 in nlp.subpipelines
+    doc2 = deployed_nlp(text2)
 
-    # a new sub-pipeline is created since text3 is on bob's machine
-    assert (len(nlp.subpipelines) == 2)
-    assert (len(nlp.pipeline['bob']) == 1)
+    # now only one extra subpipeline was created at James (Subpipeline[Tokenizer])
+    # Classfier at Bob(Subpipeline[SingleLabelClassifier]) was re-used.
+    assert len(deployed_nlp.subpipelines) == 3
+
+    subpipeline_hash3 = hash_string(string='james' + 'tokenizer')
+    assert subpipeline_hash3 in deployed_nlp.subpipelines
