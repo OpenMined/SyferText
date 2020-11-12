cc97b7964a3d92ccc9205029a580d259cc22744a
Jatin Prakash <jatinprakash1511@gmail.com>
2020-11-12 21:33:11 +0530

    black

--- tests/local_mode/test_pipeline.py
+++ tests/local_mode/test_pipeline.py
@@ -12,11 +12,12 @@ hook = sy.TorchHook(torch)
 me = hook.local_worker
 me.is_client_worker = False
 
-alice = sy.VirtualWorker(hook, id='alice')
-bob = sy.VirtualWorker(hook, id='bob')
-james = sy.VirtualWorker(hook, id='james')
-dan = sy.VirtualWorker(hook, id = 'dan')
-bill = sy.VirtualWorker(hook, id = 'bill')
+alice = sy.VirtualWorker(hook, id="alice")
+bob = sy.VirtualWorker(hook, id="bob")
+james = sy.VirtualWorker(hook, id="james")
+dan = sy.VirtualWorker(hook, id="dan")
+bill = sy.VirtualWorker(hook, id="bill")
+
 
 class simpleNet(sy.Plan):
     def __init__(self):
@@ -28,19 +29,22 @@ class simpleNet(sy.Plan):
         probs = F.relu(logits)
         return probs, logits
 
+
 doc_encoder = AverageDocEncoder()
 # Create some temporary labels
-labels = ['A', 'B']
+labels = ["A", "B"]
+
 
 def reset_object_store(worker):
     keys = list(worker._objects)
     for k in keys:
         del worker._objects[k]
 
+
 def test_pipeline_reuse():
 
-    text1 = String('This string is on alice').send(alice)
-    text2 = String('This string is on james').send(james)
+    text1 = String("This string is on alice").send(alice)
+    text2 = String("This string is on james").send(james)
 
     nlp = get_test_language_model()
     pipe_name = nlp.pipeline_name
@@ -48,13 +52,10 @@ def test_pipeline_reuse():
     # Set up SingleLabelClassifier and add to pipeline
     model = simpleNet()
     classifier = SingleLabelClassifier(
-        classifier=model, 
-        doc_encoder=doc_encoder,
-        encryption='mpc',
-        labels=labels,
-    )  
+        classifier=model, doc_encoder=doc_encoder, encryption="mpc", labels=labels,
+    )
 
-    nlp.add_pipe(classifier, name='test_classifier', access={'bob'})
+    nlp.add_pipe(classifier, name="test_classifier", access={"bob"})
     nlp.deploy(bob)
 
     # Clear my object store
@@ -67,10 +68,10 @@ def test_pipeline_reuse():
     # Now 2 subpipelines were created, one for Tokenizer at Alice (Subpipeline[Tokenizer]), and one for Classfier at Bob(Subpipeline[SingleLabelClassifier])
     assert len(deployed_nlp.subpipelines) == 2
 
-    subpipeline_hash1 = hash_string(string='alice' + 'tokenizer')
+    subpipeline_hash1 = hash_string(string="alice" + "tokenizer")
     assert subpipeline_hash1 in deployed_nlp.subpipelines
 
-    subpipeline_hash2 = hash_string(string='bob' + 'test_classifier')
+    subpipeline_hash2 = hash_string(string="bob" + "test_classifier")
     assert subpipeline_hash2 in deployed_nlp.subpipelines
 
     doc2 = deployed_nlp(text2)
@@ -79,5 +80,5 @@ def test_pipeline_reuse():
     # Classfier at Bob(Subpipeline[SingleLabelClassifier]) was re-used.
     assert len(deployed_nlp.subpipelines) == 3
 
-    subpipeline_hash3 = hash_string(string='james' + 'tokenizer')
+    subpipeline_hash3 = hash_string(string="james" + "tokenizer")
     assert subpipeline_hash3 in deployed_nlp.subpipelines
