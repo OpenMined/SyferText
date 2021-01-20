dc55c01dd45f1039234d847de954f6cae7f822f9
Hersh Dhillon <hershdhillon23@gmail.com>
2021-01-21 02:33:27 +0530

    Implementing caching when loading from PyGrid nodes (#191)
    * Update SyferText for Pygrid compatibility
    
        Got tutorials running on grid nodes,
        add protocol cmnds for encrypted vector
    
    * Bump up Pysyft version to 0.2.9
    
    * point syft to tag0.2.9
    
    * Apply suggestions from code review
    
    * Init updated
    
    * File route changed
    
    * black formatting
    
    * Updated cache location
    
    * Tested pipeline on virtual workers
    
    * Loading from cache functionality completed
    
    * Caching test v1
    
    * Adding Pysyft branch
    
    * Added type tuple in single_label_classifer
    
    * Resolving suggestions
    
    * Black formatting the changes
    
    * Removed redundant lines and improved test
    
    * Disabled the faulty test for the time being and removed cache_pipeline.py from tutorials
    
    * Retrying CI
    
    Co-authored-by: Nilanshrajput <2017csb1092@iitrpr.ac.in>
    Co-authored-by: Alan Aboudib <agabudeeb@gmail.com>

--- syfertext/__init__.py
+++ syfertext/__init__.py
@@ -2,6 +2,9 @@ import syft
 from syft.serde.msgpack.serde import msgpack_global_state
 from syft.workers.base import BaseWorker
 import torch
+import dill
+import os
+from pathlib import Path
 
 # Get a torch hook
 HOOK = syft.TorchHook(torch)
@@ -13,9 +16,11 @@ from .language import Language
 from .pipeline import SubPipeline
 from .pipeline import Pipeline
 from .pipeline.pointers.pipeline_pointer import PipelinePointer
-from .utils import search_resource
+from .pointers.state_pointer import StatePointer
+from .utils import search_resource, create_state_query
 
 from typing import Set
+from typing import Union
 
 
 def load(pipeline_name: str) -> Language:
@@ -31,6 +36,8 @@ def load(pipeline_name: str) -> Language:
     # Search for the pipeline
     result = search_resource(query=pipeline_name, local_worker=LOCAL_WORKER)
 
+    pipeline = None
+
     # If no pipeline is found, return
     if not result:
         return
@@ -45,6 +52,9 @@ def load(pipeline_name: str) -> Language:
         # Get a copy of the pipeline using its pointer
         pipeline = result.get_copy()
 
+        # Save the pipeline to local storage
+        save(pipeline_name=pipeline_name, pipeline=pipeline, destination="local")
+
     elif isinstance(result, Pipeline):
 
         # In this case, the Pipeline object is found on the local worker
@@ -54,6 +64,18 @@ def load(pipeline_name: str) -> Language:
 
         # Get the pipeline object
         pipeline = result
+
+    elif isinstance(result, tuple):
+
+        # In this case we get a simplified pipeline object,
+        # from the stored cache which is a tuple.
+        # The following code details it back to a pipeline object.
+
+        pipeline = Pipeline.detail(worker=LOCAL_WORKER, pipeline_simple=result)
+
+        # Since we found the model in local storage, deployed_on = None
+
+        deployed_on = None
 
     # Instantiate a Language object
     nlp = Language(
@@ -97,3 +119,55 @@ def create(pipeline_name, tags: Set[str] = None, description: str = None):
 
 # Set the default owners of some classes
 # SubPipeline.owner = LOCAL_WORKER
+
+
+def save(pipeline_name: str, pipeline: "Pipeline", destination: Union["local"] = "local") -> None:
+    """Saves the pipeline and it's states to storage
+
+    Args:
+        pipeline_name (str): The name of the pipeline.
+        pipeline (Pipeline): The pipeline object itself
+        destination (Union): The location where to save that object, currently storage on local machine is implemented.
+
+    """
+
+    # Path to the home/SyferText/cache/<pipeline_name> directory
+    data_path = os.path.join(str(Path.home()), "SyferText", "cache", pipeline_name)
+
+    # Creating a new directory if home/SyferText/cache/<pipeline_name> does not exist
+    if not os.path.exists(data_path):
+        os.makedirs(data_path, exist_ok=True)
+
+    # Making a target at the file
+    target = str("/{}.pkl".format(pipeline_name))
+
+    # Opening cache file
+    pipeline_cache = open(data_path + target, "wb")
+
+    # Dumping data
+    dill.dump(pipeline.simplify(worker=LOCAL_WORKER, pipeline=pipeline), pipeline_cache)
+
+    # Loading states
+    for state in pipeline.states_info:
+        # Searching on state
+        state_id = create_state_query(pipeline_name=pipeline_name, state_name=state)
+
+        result = search_resource(query=state_id, local_worker=LOCAL_WORKER)
+
+        if isinstance(result, StatePointer):
+            # State target
+
+            # Since : is a reserved character for naming files, replacing with - instead
+            # Splitting tokens to get the name of the pipeline for the name of the pipeline directory
+
+            file_name = state_id.replace(":", "-")
+
+            target = str("/{}.pkl".format(file_name))
+
+            state_obj = result.get_copy()
+
+            state_cache = open(data_path + target, "wb")
+
+            dill.dump(state_obj.simplify(worker=LOCAL_WORKER, state=state_obj), state_cache)
+
+    pipeline_cache.close()
---
+++ tests/local_mode/test_cache.py
@@ -0,0 +1,64 @@
+import syft as sy
+import torch
+import syfertext
+import os
+from pathlib import Path
+
+# Import PySyft's String class
+from syft.generic.string import String
+from syfertext.local_pipeline import get_test_language_model
+
+hook = sy.TorchHook(torch)
+me = hook.local_worker
+me.is_client_worker = False
+
+
+def reset_object_store(worker):
+    """ This functions resets all the objects for the specified worker """
+    keys = list(worker._objects)
+    for k in keys:
+        del worker._objects[k]
+
+
+# Remote Virutal Workers
+bob = sy.VirtualWorker(hook, id="bob")
+alice = sy.VirtualWorker(hook, id="alice")
+
+nlp = get_test_language_model()
+nlp.deploy(bob)
+
+# Resetting the object store
+reset_object_store(me)
+
+
+def test_save_to_storage():
+
+    cache_directory = os.path.join(str(Path.home()), "SyferText", "cache", "syfertext_sentiment")
+
+    # Remove any existing directory
+    if os.path.exists(cache_directory):
+        for f in os.listdir(cache_directory):
+            os.remove(cache_directory + "/" + f)
+
+        os.rmdir(cache_directory)
+
+    nlp_to_save = syfertext.load("syfertext_sentiment")
+
+    # Assert that the directory now exists
+    assert os.path.exists(cache_directory)
+
+    # Given that the directory now exists load it again from cache
+
+    nlp_to_load = syfertext.load("syfertext_sentiment")
+
+    assert nlp_to_save.pipeline_template == nlp_to_load.pipeline_template
+    assert nlp_to_save.states_info == nlp_to_load.states_info
+
+    cache_directory = os.path.join(str(Path.home()), "SyferText", "cache", "syfertext_sentiment")
+
+    # Remove the added directory after test
+    if os.path.exists(cache_directory):
+        for f in os.listdir(cache_directory):
+            os.remove(cache_directory + "/" + f)
+
+        os.rmdir(cache_directory)
