249ece3f425de1290a363467305ebc88b9f5b6f1
Fernando Dorado <fdorado.rueda@gmail.com>
2021-05-02 13:50:41 +0200

    change pack name

--- setup.py
+++ setup.py
@@ -1,34 +1,21 @@
+"""
+    Setup file for SyferText.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 4.0.1.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
 from setuptools import setup
-from setuptools import find_packages
-import os
 
-# Utility function to read README.md file for long description
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name="syfertext",
-    author="Alan Aboudib",
-    author_email="agabudeeb@gmail.com",
-    description="A privacy preserving NLP framework",
-    url="https://github.com/OpenMined/SyferText",
-    keywords="nlp smpc secure multi-party computation federated learning deep learning artificial intelligence secure model sharin natural language processing spacy spaCy",
-    classifier=["Programming Language :: Python :: 3.7", "Operating System :: OS Independent"],
-    license="Apache-2.0",
-    version="0.2.0",
-    packages=find_packages(),
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    install_requires=["tqdm==4.36.1", "mmh3==2.5.1", "requests==2.22.0", "syft==0.5.0rc1"],
-    extras_require={
-        "test": [
-            "black>=20.8b1",
-            "pytest>=5.3.5",
-            "pytest-black>=0.3.8",
-            "jupyter>=1.0.0",
-            "papermill>=1.2.1",
-        ]
-    },
-    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
-)
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
