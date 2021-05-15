b6a63b24175cf0528451eb830462e10673bbf8ca
FernandoDorado <inanoxtm@gmail.com>
2021-05-15 12:58:30 +0200

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -245,7 +245,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "SyferText Documentation", "Fernando Dorado", "manual")
+    ("index", "user_guide.tex", "SyferText Documentation", "Alan Aboudib", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
